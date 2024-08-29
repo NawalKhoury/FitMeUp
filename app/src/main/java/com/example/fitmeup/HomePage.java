abc7105be7162c094de88deed1e904b89fa5041b
NawalKhoury <nawalkhoury20@gmail.com>
2024-08-29 12:14:50 +0300

    Home Page

--- app/src/main/java/com/example/fitmeup/HomePage.java
+++ app/src/main/java/com/example/fitmeup/HomePage.java
@@ -1,7 +1,6 @@
 package com.example.fitmeup;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,34 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.bumptech.glide.Glide;
-
-
-public class MainActivity extends AppCompatActivity {
+public class HomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-
-        ImageView imageView = findViewById(R.id.imageView);
-
+        setContentView(R.layout.activity_home_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        // Load GIF using Glide
-        Glide.with(this)
-                .asGif()
-                .load(R.drawable.logo)  // Ensure the logo.gif is correctly placed in res/drawable
-                .into(imageView);
     }
-
-
-
-
 }
