a24bf92b60d605776b47756f7cd072f25bd58e07
abed2809 <135070794+abed2809@users.noreply.github.com>
2024-09-01 11:18:03 +0300

    1) delete home activity 2)edit log-in page

--- app/src/main/java/com/example/fitmeup/MainActivity.java
+++ app/src/main/java/com/example/fitmeup/MainActivity.java
@@ -4,22 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
-import android.widget.Button;
-
-import android.view.View;
-import android.widget.Button;
-
-import android.widget.ImageView;
 
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import com.bumptech.glide.Glide;
-
 
 import com.bumptech.glide.Glide;
 
