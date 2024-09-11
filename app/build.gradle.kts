9c5c66693a2c32cee2230ade92507c040249c5af
DenaYahya <155540636+DenaYahya@users.noreply.github.com>
2024-09-11 09:37:03 +0300

    raneem was here

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,6 @@ android {
     }
 }
 
-
-
 dependencies {
     implementation("androidx.viewpager2:viewpager2:1.1.0") // Use the latest version available
     implementation("androidx.room:room-runtime:2.5.2")
@@ -45,10 +43,26 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+    annotationProcessor("androidx.room:room-compiler:2.5.2")
+
+    // Glide for image loading
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+
+    // AndroidX Libraries
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Testing dependencies
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(libs.constraintlayout)
     implementation(libs.glide)
     annotationProcessor(libs.glide)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-}
+    on("androidx.test.espresso:espresso-core:3.5.1")}
